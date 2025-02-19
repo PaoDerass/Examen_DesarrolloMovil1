825b4d4addff6c7cde7c26f6b72c65e1ae9b4787
PaoDerass <paola_deras@unitec.edu>
2025-02-18 19:23:57 -0600

    configuracion inicio

---
+++ restaurante_app/src/pantallas/Inicio.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+export default function Inicio() {
+  return (
+    <View>
+      <Text>Lista de platos disponibles</Text>
+      <Button title='Agregar Pedido' />
+    </View>
+  );
+}
