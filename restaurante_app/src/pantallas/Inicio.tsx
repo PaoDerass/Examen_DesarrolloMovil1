415eb56702c252a8f44ccf4842afab51c4fb93c3
PaoDerass <paola_deras@unitec.edu>
2025-02-18 19:34:47 -0600

    agregar platos

--- restaurante_app/src/pantallas/Inicio.tsx
+++ restaurante_app/src/pantallas/Inicio.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, FlatList } from 'react-native';
+
+const platos = [
+  { id: '1', nombre: 'Lasaña', precio: '150.00' },
+  { id: '2', nombre: 'Pasta Alfredo', precio: '120.50' },
+  { id: '3', nombre: 'Risotto', precio: '180.00' },
+  { id: '4', nombre: 'Gelato', precio: '110.00' },
+  { id: '5', nombre: 'pizza', precio: '100.00' }
+];
 
 export default function Inicio() {
   return (
     <View>
       <Text>Lista de platos disponibles</Text>
-      <Button title='Agregar Pedido' />
+      <FlatList
+        data={platos}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.nombre} - ${item.precio}</Text>
+            <Button title='Agregar Pedido' />
+          </View>
+        )}
+      />
     </View>
   );
 }
