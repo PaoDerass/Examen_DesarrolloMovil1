73deeec506be828f98be110e651e5537038da0d0
PaoDerass <paola_deras@unitec.edu>
2025-02-18 22:12:39 -0600

    app.tsx

--- restaurante_app/App.tsx
+++ restaurante_app/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import Navegacion from './src/navegacion/Navegacion';
+import { PedidoProvider } from './src/context/PedidoContext';
 
 export default function App() {
   return (
-    <>
+    <PedidoProvider>
       <StatusBar barStyle="dark-content" />
       <Navegacion />
-    </>
+    </PedidoProvider>
   );
 }
